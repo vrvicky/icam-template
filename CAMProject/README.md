bfcfad7bd91e2e06ae573c2a34d182d65133c28c
Vignesh <vr_vignesh@yahoo.com>
2018-05-08 10:54:34 +0000

    delete

--- CAMProject/README.md
+++
@@ -1,8 +0,0 @@
-## CAMProject infrastructure
-
-Created 5/8/18 by ucdpadmin. for CAMProject
-
-
-## Design Decisions
-| Design item                | Decision|
-| :----------------------------------- | :--------------------------------------------------------------------------------|
--- CAMProject/outputs.tf
+++
@@ -1,9 +0,0 @@
-#####################################################################
-##
-##      Created 5/8/18 by ucdpadmin. for CAMProject
-##
-#####################################################################
-
-output "web_server_ip_address" {
-  value = "${aws_instance.web_server.public_ip}"
-}
