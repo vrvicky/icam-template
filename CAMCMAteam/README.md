bde45ae956394d03836aa243f4e7e378ded15ece
vignesh <vrvignesh@in.ibm..com>
2018-08-02 07:13:39 +0000

    first commit

---
+++ CAMCMAteam/README.md
@@ -0,0 +1,8 @@
+## CAMCMAteam infrastructure
+
+Created 8/2/18 by admin. for CAMCMAteam
+
+
+## Design Decisions
+| Design item                | Decision|
+| :----------------------------------- | :--------------------------------------------------------------------------------|
---
+++ CAMCMAteam/variables.tf
@@ -0,0 +1,44 @@
+#####################################################################
+##
+##      Created 8/2/18 by admin. for CAMCMAteam
+##
+#####################################################################
+
+
+
+variable "region" {
+  type = "string"
+  description = "Generated"
+  default     = "us-west-2"
+}
+
+variable "web-server_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "web-server_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "web-server_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "subnet_id" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "security_group_id" {
+  type = "string"
+  description = "The associated security groups."
+}
+
